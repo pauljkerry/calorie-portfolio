59da38154857a657cb550f6a1350881056424e9b
pauljkerry <hanseikai2019@gmail.com>
2025-07-19 05:17:00 +0900

    adding logreg optimizer

---
+++ src/models/logreg/logreg_optuna_optimizer.py
@@ -0,0 +1,81 @@
+import optuna
+from src.models.logreg.logreg_cv_trainer import LogRegCVTrainer
+
+
+def create_objective(tr_df, n_splits=5):
+    """
+    Optunaの目的関数（objective）を生成する関数。
+
+    Parameters
+    ----------
+    tr_df : cudf.DataFrame
+        訓練データ。
+    n_splits : int, default 5
+        CV分割数。
+
+    Returns
+    -------
+    objective : function
+        optunaで使用する目的関数。
+    """
+    def objective(trial):
+        params = {
+            "C": trial.suggest_float("C", 1e-2, 1e2, log=True)
+        }
+
+        trainer = LogRegCVTrainer(
+            params=params,
+            n_splits=n_splits
+        )
+
+        trainer.fit_one_fold(tr_df, fold=0)
+
+        return trainer.fold_scores[0]
+    return objective
+
+
+def run_optuna_search(
+    objective, n_trials=50, study_name="lgbm_study",
+    storage=None, initial_params: dict = None, sampler=None
+):
+    """
+    Optunaによるハイパーパラメータ探索を実行する関数。
+
+    Parameters
+    ----------
+    objective : function
+        Optunaの目的関数。
+    n_trials : int, default 50
+        試行回数。
+    study_name : str or None, default "lgbm_study"
+        StudyName。
+    storage : str or None, default None
+        保存先URL。
+    initial_params : dict or None, default None
+        初期の試行パラメータ。
+    sampler : optuna.samplers.BaseSampler or None, default TPESampler
+        使用するSampler。
+
+    Returns
+    -------
+    study : optuna.Study
+        探索結果のStudyオブジェクト。
+    """
+    study = optuna.create_study(
+        direction="minimize",
+        study_name=study_name,
+        storage=storage,
+        load_if_exists=True,
+        sampler=sampler or optuna.samplers.TPESampler()
+    )
+
+    if initial_params is not None:
+        study.enqueue_trial(initial_params)
+
+    study.optimize(
+        objective,
+        n_trials=n_trials,
+        show_progress_bar=True
+    )
+
+    return study
